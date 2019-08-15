e9b7278f5a1feb47aa8c3fee5a169a4b2067f753
Luis Parra <luis.parra.pulido@gmail.com>
2019-08-15 14:50:21 -0500

    Modulo asincronismo

--- programacion_orientada_a_objetos_en_javascript.js
+++ programacion_orientada_a_objetos_en_javascript.js
@@ -37,17 +37,54 @@
 //   console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
 // }
 
+// class Persona {
+//   constructor(nombre, apellido, altura) {
+//     this.nombre = nombre
+//     this.apellido = apellido
+//     this.altura = altura
+//     }
+//
+//     saludar() {
+//       console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+//     }
+//
+//     soyAlto() {
+//       if(this.altura > 1.7) {
+//         console.log('Soy alto')
+//       }else {
+//         console.log('Soy bajo')
+//       }
+//     }
+//
+// }
+//
+//
+// class Desarrollador extends Persona {
+//   constructor(nombre, apellido, altura){
+//     super(nombre, apellido, altura)
+//   }
+//
+//   saludar(){
+//     console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
+//   }
+//
+// }
+
+// var luis = new Persona('luis', 'parra', 1.72)
+//var ale = new Desarrollador('ale', 'pila', 1.70)
+
 class Persona {
   constructor(nombre, apellido, altura) {
     this.nombre = nombre
     this.apellido = apellido
     this.altura = altura
     }
-
-    saludar() {
+    saludar(fn) {
       console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+      if(fn){
+        fn(this.nombre, this.apellido, false)
+      }
     }
-
     soyAlto() {
       if(this.altura > 1.7) {
         console.log('Soy alto')
@@ -55,20 +92,30 @@ class Persona {
         console.log('Soy bajo')
       }
     }
-
 }
-
 
 class Desarrollador extends Persona {
   constructor(nombre, apellido, altura){
     super(nombre, apellido, altura)
   }
-
-  saludar(){
+  saludar(fn){
     console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
+    if(fn){
+      fn(this.nombre, this.apellido, true)
+    }
   }
-
 }
 
-// var luis = new Persona('luis', 'parra', 1.72)
-//var ale = new Desarrollador('ale', 'pila', 1.70)
+function responderSaludo(nombre, apellido, esDev) {
+  console.log(`Buen día ${nombre} ${apellido}`)
+  if(esDev){
+    console.log(`Ah mira, no sabia que eras desarrollador`)
+  }
+}
+
+
+var luis = new Persona('luis', 'parra', 1.72)
+var ale = new Desarrollador('ale', 'pila', 1.70)
+
+luis.saludar(responderSaludo)
+ale.saludar(responderSaludo)
